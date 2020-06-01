ca05358162a2f0b5842cbefd49faa8bc5eda78e4
kazukikazu <65213260+kazukikazu@users.noreply.github.com>
2020-06-01 23:48:53 +0900

    kazukikazu

---
+++ W05/w05_ex1.js
@@ -0,0 +1,39 @@
+function main()
+{
+    var width = 500;
+    var height = 500;
+
+    var scene = new THREE.Scene();
+
+    var fov = 45;
+    var aspect = width / height;
+    var near = 1;
+    var far = 1000;
+    var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera.position.set( 0, 0, 5 );
+    scene.add( camera );
+
+    var light = new THREE.PointLight();
+    light.position.set( 5, 5, 5 );
+    scene.add( light );
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( width, height );
+    document.body.appendChild( renderer.domElement );
+
+    var geometry = new THREE.TorusKnotGeometry( 1, 0.3, 100, 20 );
+    var material = new THREE.MeshLambertMaterial();
+
+    var torus_knot = new THREE.Mesh( geometry, material );
+    scene.add( torus_knot );
+
+    loop();
+
+    function loop()
+    {
+        requestAnimationFrame( loop );
+        torus_knot.rotation.x += 0.01;
+        torus_knot.rotation.y += 0.01;
+        renderer.render( scene, camera );
+    }
+}
